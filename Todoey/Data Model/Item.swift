df57c451506e85dd7b873e1d625f4bb738751d35
Ashish Chaturvedi <ashishchaturvedi@ashishs-imac.local>
2018-06-16 09:58:25 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ashish Chaturvedi on 2018-06-14.
+//  Copyright © 2018 Avantika Chaturvedi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
